07d12b046536b408487a4443a76f619bc95e3d6f
Thomas Roberts <t.92.roberts@gmail.com>
2018-11-23 13:48:14 +0100

    Added a method to compute the value of a complete solution (a roadmap)

--- AgileLocalSearch/main.cpp
+++ AgileLocalSearch/main.cpp
@@ -9,6 +9,8 @@ class Sprint {
 public:
 	int sprintNumber, sprintCapacity, sprintValueBonus;
 
+	Sprint() {};
+	
 	Sprint(int num, int cap, int bonus) {
 		this->sprintNumber = num;
 		this->sprintCapacity = cap;
@@ -24,19 +26,21 @@ public:
 
 class Sprints {
 public:
-	vector<Sprint> sprints;
+	vector<Sprint> sprintsData;
 
-	Sprints(vector<Sprint> sprints) {
-		this->sprints = sprints;
+	Sprints() {};
+	
+	Sprints(vector<Sprint> sprintsData) {
+		this->sprintsData = sprintsData;
 	}
 
 	string toString() {
 		string outputString = "";
 
-		for (int i = 0; i < this->sprints.size(); ++i) {
-			outputString += this->sprints[i].toString();
+		for (int i = 0; i < this->sprintsData.size(); ++i) {
+			outputString += this->sprintsData[i].toString();
 
-			if (i < this->sprints.size() - 1) {
+			if (i < this->sprintsData.size() - 1) {
 				outputString += "\n";
 			}
 		}
@@ -50,6 +54,8 @@ public:
 	int storyNumber, businessValue, storyPoints;
 	vector<Story> dependencies;
 
+	Story() {};
+	
 	Story(int storyNumber, int businessValue, int storyPoints) {
 		this->storyNumber = storyNumber;
 		this->businessValue = businessValue;
@@ -94,19 +100,21 @@ public:
 
 class Stories {
 public:
-	vector<Story> stories;
+	vector<Story> storiesData;
 
-	Stories(vector<Story> stories) {
-		this->stories = stories;
+	Stories() {};
+	
+	Stories(vector<Story> storiesData) {
+		this->storiesData = storiesData;
 	}
 
 	string toString() {
 		string outputString = "";
 
-		for (int i = 0; i < this->stories.size(); ++i) {
-			outputString += this->stories[i].toString();
+		for (int i = 0; i < this->storiesData.size(); ++i) {
+			outputString += this->storiesData[i].toString();
 
-			if (i < this->stories.size() - 1) {
+			if (i < this->storiesData.size() - 1) {
 				outputString += "\n";
 			}
 		}
@@ -117,21 +125,52 @@ public:
 
 class Roadmap {
 public:
-	vector<int> roadmap;
+	Sprints sprints;
+	Stories stories;
+	vector<int> assignments;
 
-	Roadmap(vector<int> roadmap) {
-		this->roadmap = roadmap;
+	Roadmap(Sprints sprints, Stories stories, vector<int> assignments) {
+		this->sprints = sprints;
+		this->stories = stories;
+		this->assignments = assignments;
+	}
+
+	int computeValue() {
+		int totalValue = 0;
+
+		for (int i = 0; i < this->assignments.size(); ++i) {
+			int storyNumber = i;
+			int assignedSprint = this->assignments[i];
+
+			if (assignedSprint > -1) { // Don't add a story's business value if it's not assigned to a sprint
+				int storyBusinessValue = stories.storiesData[i].businessValue;
+				int sprintBonus = sprints.sprintsData[assignedSprint].sprintValueBonus;
+
+				totalValue += storyBusinessValue * sprintBonus;
+			}
+		}
+
+		return totalValue;
 	}
 
 	string toString() {
 		string outputString = "";
 
-		for (int i = 0; i < this->roadmap.size(); ++i) {
+		for (int i = 0; i < this->assignments.size(); ++i) {
+			int storyNumber = i;
+			int assignedSprint = this->assignments[i];
+
 			outputString += "Story " + to_string(i);;
-			if (this->roadmap[i] == -1) {
-				outputString += " => No sprint\n";
+			if (assignedSprint == -1) {
+				outputString += " => No sprint";
+				outputString += " (value: 0)\n";
 			} else {
-				outputString += " => Sprint " + to_string(this->roadmap[i]) + "\n";
+				outputString += " => Sprint " + to_string(assignedSprint);
+
+				int storyBusinessValue = stories.storiesData[i].businessValue;
+				int sprintBonus = sprints.sprintsData[assignedSprint].sprintValueBonus;
+
+				outputString += " (value: " + to_string(storyBusinessValue) + " * " + to_string(sprintBonus) + " = " + to_string(storyBusinessValue * sprintBonus) + ")\n";
 			}
 		}
 
@@ -159,6 +198,7 @@ int main(int argc, char* argv[]) {
 	sprintData.push_back(sprint1);
 
 	Sprints sprints = Sprints(sprintData);
+	cout << "Sprints:" << endl;
 	cout << sprints.toString() << endl << endl;
 
 	/////////////////////////////////////////////////////////////////
@@ -176,6 +216,7 @@ int main(int argc, char* argv[]) {
 	storyData.push_back(story2);
 
 	Stories stories = Stories(storyData);
+	cout << "Stories:" << endl;
 	cout << stories.toString() << endl << endl;
 
 	/////////////////////////////////////////////////////////////////
@@ -187,6 +228,8 @@ int main(int argc, char* argv[]) {
 	assignment.push_back(randomInt(-1, numberOfSprints - 1));
 	assignment.push_back(randomInt(-1, numberOfSprints - 1));
 
-	Roadmap roadmap = Roadmap(assignment);
-	cout << roadmap.toString();
+	Roadmap roadmap = Roadmap(sprints, stories, assignment);
+	cout << "Roadmap:" << endl;
+	cout << roadmap.toString() << endl;
+	cout << "Value: " << roadmap.computeValue();
 }
